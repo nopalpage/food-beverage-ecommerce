4a37d0d25d99f1d88bdf4336048bda747b43c94e
noval <55214891+nopalpage@users.noreply.github.com>
2025-11-09 04:08:29 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,20 @@
+// pages/_app.js
+import '@/styles/globals.css';
+import { useEffect } from 'react';
+import Layout from '@/components/Layout';
 
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js');
+    }
+  }, []);
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
