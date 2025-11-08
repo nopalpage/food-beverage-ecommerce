498e4a10ae54d5441a3f3f1656c0e2a490bbd980
noval <55214891+nopalpage@users.noreply.github.com>
2025-11-09 04:11:51 +0900

    Create offline.js

---
+++ pages/offline.js
@@ -0,0 +1,8 @@
+export default function Offline() {
+  return (
+    <div className="text-center py-20">
+      <h1 className="text-2xl font-bold">Kamu sedang offline.</h1>
+      <p>Silakan kembali online untuk melanjutkan.</p>
+    </div>
+  );
+}
